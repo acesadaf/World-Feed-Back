c0fd902b3c4a1d055d6fdd291be512ac6165711b
Sadaf Halim <acesadaf@gmail.com>
2020-08-12 00:53:44 -0500

    heroku

--- backend/views.py
+++ backend/views.py
@@ -19,7 +19,7 @@ news_api_key = "c3677ea6a98b4e13a34afccee2199218"
 
 def get_tweet(request):
     tweets = []
-    for tweet in tweepy.Cursor(api.search, q="#" + "tweeting" + " -filter:retweets", rpp=5, lang="en", tweet_mode="extended").items(10):
+    for tweet in tweepy.Cursor(api.search, q="#" + "tweeting" + " -filter:retweets", rpp=5, lang="en", tweet_mode="extended").items(1):
         print(tweet)
 
     return HttpResponse("OK")
