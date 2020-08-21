b2da669a24e218f49a73bd5065ae101979c79581
Sadaf Halim <acesadaf@gmail.com>
2020-08-20 22:58:39 -0500

    1) added caching 2) made tweet retrievals truly location specific 3) made sure returned tweets are the most popular ones if available 4)refactored a lot of code

--- backend/models.py
+++ backend/models.py
@@ -10,4 +10,16 @@ class CountryTweetCache(models.Model):
     def __str__(self):
         return f"{self.country, self.tweets}"
 
-        # Create your models here.
+
+class CityTweetCache(models.Model):
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    tweets = ArrayField(models.CharField(max_length=200))
+    cache_time = models.DateTimeField(auto_now_add=True, blank=True)
+
+    class Meta:
+        unique_together = (('city', 'country'),)
+
+    def __str__(self):
+        return f"{self.city, self.country, self.tweets}"
+    # Create your models here.
--- backend/views.py
+++ backend/views.py
@@ -4,7 +4,9 @@ from django.views.decorators.csrf import csrf_exempt
 import tweepy
 import requests
 import json
+import reverse_geocoder as rg
 from django.http import JsonResponse
+from backend.models import CountryTweetCache, CityTweetCache
 consumer_key = "I7VpkNuclA6KETbxU1fj2IIav"
 consumer_secret = "9GoP6ZEbBRpCEPyBZ4GiWomgvDwBjGnat0iwIJBexMRVvq2pHm"
 access_token = "1007201002930335744-kbdzzTCMrjRZIGq155Teu2MBASozOb"
@@ -15,24 +17,129 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 
 news_api_key = "c3677ea6a98b4e13a34afccee2199218"
 
-
 # Create your views here.
+
+
+def cache_city(city, country, tweets):
+    ctc = CityTweetCache(city=city, country=country, tweets=tweets)
+    ctc.save()
+
+
+def fetch_city_cache(city, country):
+    tweets = []
+    cached_tweets = CityTweetCache.objects.get(
+        city=city, country=country)
+    print("fetching from city cache!")
+    for tweet in cached_tweets.tweets:
+        split_tweet = tweet.split("|||")
+        tweets.append({
+            'username': split_tweet[0],
+            'tweet': split_tweet[1],
+            'url': split_tweet[2]
+        })
+
+    return tweets
+
+
+def fetch_country_cache(country):
+    tweets = []
+    cached_tweets = CountryTweetCache.objects.get(country=country)
+    print("fetching from country cache!")
+    for tweet in cached_tweets.tweets:
+        split_tweet = tweet.split("|||")
+        tweets.append({
+            'username': split_tweet[0],
+            'tweet': split_tweet[1],
+            'url': split_tweet[2]
+        })
+    return tweets
+
+
+def cache_country(country, tweets):
+    ctc = CountryTweetCache(country=country, tweets=tweets)
+    ctc.save()
+
+
+def prepare_tweets(place_id):
+    print("API call!")
+    tweets, to_cache = api_call(place_id=place_id, popular=True, tweet_count=5)
+    if len(tweets) < 5:
+        remaining_tweets, remaining_cache = api_call(
+            place_id=place_id, popular=False, tweet_count=5 - len(tweets))
+
+        tweets.extend(remaining_tweets)
+        to_cache.extend(remaining_cache)
+
+    return tweets, to_cache
+
+
+def api_call(place_id, popular, tweet_count):
+    tweets = []
+    to_cache = []
+    if popular:
+        call = tweepy.Cursor(api.search, result_type="popular",
+                             q="place:{} -filter:retweets".format(place_id)).items(tweet_count)
+    else:
+        call = tweepy.Cursor(
+            api.search, q="place:{} -filter:retweets".format(place_id)).items(tweet_count)
+    for tweet in call:
+        u, t, url = tweet.user.screen_name, tweet._json['text'], "https://twitter.com/{}/status/{}".format(
+            tweet.user.screen_name, tweet.id)
+        tweets.append({'username': u,
+                       'tweet': t, 'url': url})
+        tweet_to_cache = "{}|||{}|||{}".format(
+            u, t, url)
+        to_cache.append(tweet_to_cache)
+
+    return tweets, to_cache
+
+
+def api_fetch_and_cache(body, city, country, granularity):
+    # using Twitter geocoder here, necessary to get place_id for the twitter call
+    try:
+        places = api.reverse_geocode(
+            lat=body['lat'], long=body['lng'], granularity=granularity)
+        place_id = places[0].id
+
+        tweets, to_cache = prepare_tweets(place_id)
+        if granularity == "country":
+            cache_country(country, to_cache)
+
+        else:
+            cache_city(city, country, to_cache)
+
+    except:
+        tweets = [{'username': "World Feed",
+                   'tweet': "Sorry, No tweets from here :(", 'url': "http://localhost:3000/"}]
+
+    return tweets
+
 
 @csrf_exempt
 def get_tweet(request):
-    print(request)
     body = json.loads(request.body)
-    print(body)
     tweets = []
-    # for tweet in tweepy.Cursor(api.search, q="#" + "tweeting" + " -filter:retweets", rpp=5, lang="en", tweet_mode="extended").items(1):
-    #     pass
-    # print(tweet)
-    for tweet in tweepy.Cursor(api.search, q=" -filter:retweets", geocode="{},{},500km".format(body['lat'], body['lng'])).items(4):
-        print(tweet._json['text'], tweet.user.screen_name)
-        tweets.append({'username': tweet.user.screen_name,
-                       'tweet': tweet._json['text']})
-        print("-----------------")
-        # print(tweet._json['text'])
+    # using an offline reverse geocoder here to minimize API calls
+    coordinates = (float(body['lat']), float(body['lng']))
+    location_names = rg.search(coordinates)
+    city, country = location_names[0]['admin1'], location_names[0]['cc']
+    typ = body["type"]
+    print(city, country)
+
+    if typ == "country":
+        if CountryTweetCache.objects.filter(country=country).exists():
+            tweets = fetch_country_cache(country)
+
+        else:
+            tweets = api_fetch_and_cache(body, city, country, "country")
+
+    else:
+        if CityTweetCache.objects.filter(city=city, country=country).exists():
+            tweets = fetch_city_cache(city=city, country=country)
+
+        else:
+            tweets = api_fetch_and_cache(body, city, country, "city")
+
     return JsonResponse(tweets, safe=False)
 
 
@@ -58,7 +165,7 @@ def get_videos(request):
 
     r = requests.get(search_url, params=params, verify=False)
 
-    #r = requests.get(videos_url, params=params, verify=False)
+    # r = requests.get(videos_url, params=params, verify=False)
     result = r.json()
     links = []
     for video in result['items']:
